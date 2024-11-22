1c55860385f1bf02078a5741dfb9bd217fa071aa
Fefe Alves <monitore.falves@gmail.com>
2024-11-22 00:29:25 -0300

    Add news tests to /products

--- test.py
+++ test.py
@@ -11,3 +11,12 @@ def test_hello_world():
 def test_hello_world_message():
     response = client.get("/")
     assert response.json() == {"Message": "Hello World!"}
+
+def test_products():
+    response = client.get("/products")
+    assert response.status_code == 200
+
+def test_length_products():
+    response = client.get("/products")
+    assert len(response.json()) == 3
+
